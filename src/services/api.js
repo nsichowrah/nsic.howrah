e3d7dda3cf1fce82066f9bcd1d31cb372ca2b8dd
NSIC Howrah <nsic.howrah2000@gmail.com>
2026-02-24 10:05:02 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: https://backend-ten-alpha-44.vercel.app/
+  
 });
 
 export const getLatestSensorData = () =>
